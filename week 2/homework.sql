5740f278b58685d162e5f50e8d9fcbdb360faf6d
Duong Bach Chi <164162779+ImAno177@users.noreply.github.com>
2024-10-16 08:34:49 +0700

    Update homework.sql
    Sửa lỗi unicode

--- week 2/homework.sql
+++ week 2/homework.sql
@@ -1,94 +1,94 @@
--- Li?t k� t?t c? c�c chuy�n gia v� s?p x?p theo h? t�n.
+-- Liệt kê tất cả các chuyên gia và sắp xếp theo họ tên.
 SELECT * FROM ChuyenGia ORDER BY HoTen;
--- Hi?n th? t�n v� s? ?i?n tho?i c?a c�c chuy�n gia c� chuy�n ng�nh 'Ph�t tri?n ph?n m?m'.
-SELECT HoTen, SoDienThoai FROM ChuyenGia WHERE ChuyenNganh = N'Ph�t tri?n ph?n m?m';
 
+-- Hiển thị tên và số điện thoại của các chuyên gia có chuyên ngành 'Phát triển phần mềm'.
+SELECT HoTen, SoDienThoai FROM ChuyenGia WHERE ChuyenNganh = N'Phát triển phần mềm';
 
--- Li?t k� t?t c? c�c c�ng ty c� tr�n 100 nh�n vi�n.
+-- Liệt kê tất cả các công ty có trên 100 nhân viên.
 SELECT * FROM CongTy WHERE SoNhanVien > 100;
 
--- Hi?n th? t�n v� ng�y b?t ??u c?a c�c d? �n b?t ??u trong n?m 2023.
+-- Hiển thị tên và ngày bắt đầu của các dự án bắt đầu trong năm 2023.
 SELECT TenDuAn, NgayBatDau FROM DuAn WHERE YEAR(NgayBatDau) = 2023;
 
--- Li?t k� t?t c? c�c k? n?ng v� s?p x?p theo t�n k? n?ng.
+-- Liệt kê tất cả các kỹ năng và sắp xếp theo tên kỹ năng.
 SELECT * FROM KyNang ORDER BY TenKyNang;
 
--- Hi?n th? t�n v� email c?a c�c chuy�n gia c� tu?i d??i 35 (t�nh ??n n?m 2024).
+-- Hiển thị tên và email của các chuyên gia có tuổi dưới 35 (tính đến năm 2024).
 SELECT HoTen, Email FROM ChuyenGia WHERE YEAR(2024) - YEAR(NgaySinh) < 35;
 
--- Hi?n th? t�n v� chuy�n ng�nh c?a c�c chuy�n gia n?.
-SELECT HoTen, ChuyenNganh FROM ChuyenGia WHERE GioiTinh =N'N?';
+-- Hiển thị tên và chuyên ngành của các chuyên gia nữ.
+SELECT HoTen, ChuyenNganh FROM ChuyenGia WHERE GioiTinh = N'Nữ';
 
--- Li?t k� t�n c�c k? n?ng thu?c lo?i 'C�ng ngh?'.
-SELECT * FROM KyNang WHERE TenKyNang =N'C�ng ngh?';
+-- Liệt kê tên các kỹ năng thuộc loại 'Công nghệ'.
+SELECT * FROM KyNang WHERE LoaiKyNang = N'Công nghệ';
 
--- Hi?n th? t�n v� ??a ch? c?a c�c c�ng ty trong l?nh v?c 'Ph�n t�ch d? li?u'.
-SELECT TenCongTy, DiaChi FROM CongTy WHERE LinhVuc =N'Ph�n t�ch d? li?u';
--- Li?t k� t�n c�c d? �n c� tr?ng th�i 'Ho�n th�nh'.
-SELECT TenDuAn FROM DuAn WHERE TrangThai =N'Ho�n th�nh';
+-- Hiển thị tên và địa chỉ của các công ty trong lĩnh vực 'Phân tích dữ liệu'.
+SELECT TenCongTy, DiaChi FROM CongTy WHERE LinhVuc = N'Phân tích dữ liệu';
 
--- Hi?n th? t�n v� s? n?m kinh nghi?m c?a c�c chuy�n gia, s?p x?p theo s? n?m kinh nghi?m gi?m d?n.
+-- Liệt kê tên các dự án có trạng thái 'Hoàn thành'.
+SELECT TenDuAn FROM DuAn WHERE TrangThai = N'Hoàn thành';
+
+-- Hiển thị tên và số năm kinh nghiệm của các chuyên gia, sắp xếp theo số năm kinh nghiệm giảm dần.
 SELECT HoTen, NamKinhNghiem FROM ChuyenGia ORDER BY NamKinhNghiem DESC;
 
--- Li?t k� t�n c�c c�ng ty v� s? l??ng nh�n vi�n, ch? hi?n th? c�c c�ng ty c� t? 100 ??n 200 nh�n vi�n.
+-- Liệt kê tên các công ty và số lượng nhân viên, chỉ hiển thị các công ty có từ 100 đến 200 nhân viên.
 SELECT TenCongTy, SoNhanVien FROM CongTy WHERE SoNhanVien BETWEEN 100 AND 200;
 
--- Hi?n th? t�n d? �n v� ng�y k?t th�c c?a c�c d? �n k?t th�c trong n?m 2023.
+-- Hiển thị tên dự án và ngày kết thúc của các dự án kết thúc trong năm 2023.
 SELECT TenDuAn, NgayKetThuc FROM DuAn WHERE YEAR(NgayKetThuc) = 2023;
--- Li?t k� t�n v� email c?a c�c chuy�n gia c� t�n b?t ??u b?ng ch? 'N'.
+
+-- Liệt kê tên và email của các chuyên gia có tên bắt đầu bằng chữ 'N'.
 SELECT HoTen, Email FROM ChuyenGia WHERE HoTen LIKE N'N%';
 
--- Hi?n th? t�n k? n?ng v� lo?i k? n?ng, kh�ng bao g?m c�c k? n?ng thu?c lo?i 'Ng�n ng? l?p tr�nh'.
-SELECT TenKyNang, LoaiKyNang FROM KyNang WHERE LoaiKyNang != N'Ng�n ng? l?p tr�nh';
+-- Hiển thị tên kỹ năng và loại kỹ năng, không bao gồm các kỹ năng thuộc loại 'Ngôn ngữ lập trình'.
+SELECT TenKyNang, LoaiKyNang FROM KyNang WHERE LoaiKyNang != N'Ngôn ngữ lập trình';
 
--- Hi?n th? t�n c�ng ty v� l?nh v?c ho?t ??ng, s?p x?p theo l?nh v?c.
+-- Hiển thị tên công ty và lĩnh vực hoạt động, sắp xếp theo lĩnh vực.
 SELECT TenCongTy, LinhVuc FROM CongTy ORDER BY LinhVuc;
 
--- Hi?n th? t�n v� chuy�n ng�nh c?a c�c chuy�n gia nam c� tr�n 5 n?m kinh nghi?m.
-SELECT HoTen, ChuyenNganh FROM ChuyenGia WHERE GioiTinh =N'Nam' AND NamKinhNghiem > 5;
+-- Hiển thị tên và chuyên ngành của các chuyên gia nam có trên 5 năm kinh nghiệm.
+SELECT HoTen, ChuyenNganh FROM ChuyenGia WHERE GioiTinh = N'Nam' AND NamKinhNghiem > 5;
 
--- Li?t k� t?t c? c�c chuy�n gia trong c? s? d? li?u.
+-- Liệt kê tất cả các chuyên gia trong cơ sở dữ liệu.
 SELECT * FROM ChuyenGia;
 
--- Hi?n th? t�n v� email c?a t?t c? c�c chuy�n gia n?.
-SELECT HoTen, Email FROM ChuyenGia WHERE GioiTinh = N'N?';
+-- Hiển thị tên và email của tất cả các chuyên gia nữ.
+SELECT HoTen, Email FROM ChuyenGia WHERE GioiTinh = N'Nữ';
 
---  Li?t k� t?t c? c�c c�ng ty v� s? nh�n vi�n c?a h?, s?p x?p theo s? nh�n vi�n gi?m d?n.
+-- Liệt kê tất cả các công ty và số nhân viên của họ, sắp xếp theo số nhân viên giảm dần.
 SELECT TenCongTy, SoNhanVien FROM CongTy ORDER BY SoNhanVien DESC;
 
--- Hi?n th? t?t c? c�c d? �n ?ang trong tr?ng th�i "?ang th?c hi?n".
-SELECT TenDuAn FROM DuAn WHERE TrangThai = N'?ang th?c hi?n';
+-- Hiển thị tất cả các dự án đang trong trạng thái 'Đang thực hiện'.
+SELECT TenDuAn FROM DuAn WHERE TrangThai = N'Đang thực hiện';
 
--- Li?t k� t?t c? c�c k? n?ng thu?c lo?i "Ng�n ng? l?p tr�nh".
-SELECT TenKyNang FROM KyNang WHERE LoaiKyNang = N'Ng�n ng? l?p tr�nh';
+-- Liệt kê tất cả các kỹ năng thuộc loại "Ngôn ngữ lập trình".
+SELECT TenKyNang FROM KyNang WHERE LoaiKyNang = N'Ngôn ngữ lập trình';
 
--- Hi?n th? t�n v� chuy�n ng�nh c?a c�c chuy�n gia c� tr�n 8 n?m kinh nghi?m.
+-- Hiển thị tên và chuyên ngành của các chuyên gia có trên 8 năm kinh nghiệm.
 SELECT HoTen, ChuyenNganh FROM ChuyenGia WHERE NamKinhNghiem > 8;
 
--- Li?t k� t?t c? c�c d? �n c?a c�ng ty c� MaCongTy l� 1.
+-- Liệt kê tất cả các dự án của công ty có MaCongTy là 1.
 SELECT * FROM DuAn WHERE MaCongTy = 1;
 
--- ??m s? l??ng chuy�n gia trong m?i chuy�n ng�nh.
-SELECT ChuyenNganh, COUNT(*) AS SoLuongChuyenGia
-FROM ChuyenGia
-GROUP BY ChuyenNganh;
+-- Đếm số lượng chuyên gia trong mỗi chuyên ngành.
+SELECT ChuyenNganh, COUNT(*) AS SoLuongChuyenGia FROM ChuyenGia GROUP BY ChuyenNganh;
 
--- T�m chuy�n gia c� s? n?m kinh nghi?m cao nh?t.
+-- Tìm chuyên gia có số năm kinh nghiệm cao nhất.
 SELECT TOP 1 HoTen, NamKinhNghiem FROM ChuyenGia ORDER BY NamKinhNghiem DESC;
 
--- Li?t k� t?ng s? nh�n vi�n cho m?i c�ng ty m� c� s? nh�n vi�n l?n h?n 100. S?p x?p k?t qu? theo s? nh�n vi�n t?ng d?n.
+-- Liệt kê tổng số nhân viên cho mỗi công ty mà có số nhân viên lớn hơn 100. Sắp xếp kết quả theo số nhân viên tăng dần.
 SELECT TenCongTy, SoNhanVien FROM CongTy WHERE SoNhanVien > 100 ORDER BY SoNhanVien ASC;
 
--- X�c ??nh s? l??ng d? �n m� m?i c�ng ty tham gia c� tr?ng th�i '?ang th?c hi?n'. Ch? bao g?m c�c c�ng ty c� h?n m?t d? �n ?ang th?c hi?n. S?p x?p k?t qu? theo s? l??ng d? �n gi?m d?n.
+-- Xác định số lượng dự án mà mỗi công ty tham gia có trạng thái 'Đang thực hiện'. Chỉ bao gồm các công ty có hơn một dự án đang thực hiện. Sắp xếp kết quả theo số lượng dự án giảm dần.
 SELECT CongTy.TenCongTy, COUNT(DuAn.MaDuAn) 
 FROM CongTy
 JOIN DuAn ON CongTy.MaCongTy = DuAn.MaCongTy
-WHERE DuAn.TrangThai = N'?ang th?c hi?n'
+WHERE DuAn.TrangThai = N'Đang thực hiện'
 GROUP BY CongTy.TenCongTy
 HAVING COUNT(DuAn.MaDuAn) >= 1
 ORDER BY COUNT(DuAn.MaDuAn) DESC;
 
--- T�m ki?m c�c k? n?ng m� chuy�n gia c� c?p ?? t? 4 tr? l�n v� t�nh t?ng s? chuy�n gia cho m?i k? n?ng ?�. Ch? bao g?m nh?ng k? n?ng c� t?ng s? chuy�n gia l?n h?n 2. S?p x?p k?t qu? theo t�n k? n?ng t?ng d?n.
+-- Tìm kiếm các kỹ năng mà chuyên gia có cấp độ từ 4 trở lên và tính tổng số chuyên gia cho mỗi kỹ năng đó. Chỉ bao gồm những kỹ năng có tổng số chuyên gia lớn hơn 2. Sắp xếp kết quả theo tên kỹ năng tăng dần.
 SELECT KyNang.TenKyNang, COUNT(ChuyenGia.MaChuyenGia) AS SoLuongChuyenGia
 FROM KyNang
 JOIN ChuyenGia_KyNang ON KyNang.MaKyNang = ChuyenGia_KyNang.MaKyNang
@@ -98,21 +98,21 @@ GROUP BY KyNang.TenKyNang
 HAVING COUNT(ChuyenGia.MaChuyenGia) > 2
 ORDER BY KyNang.TenKyNang ASC;
 
--- Li?t k� t�n c�c c�ng ty c� l?nh v?c '?i?n to�n ?�m m�y' v� t�nh t?ng s? nh�n vi�n c?a h?. S?p x?p k?t qu? theo t?ng s? nh�n vi�n t?ng d?n.
+-- Liệt kê tên các công ty có lĩnh vực 'Điện toán đám mây' và tính tổng số nhân viên của họ. Sắp xếp kết quả theo tổng số nhân viên tăng dần.
 SELECT TenCongTy, SUM(SoNhanVien) AS TongSoNhanVien
 FROM CongTy
-WHERE LinhVuc = N'?i?n to�n ?�m m�y'
+WHERE LinhVuc = N'Điện toán đám mây'
 GROUP BY TenCongTy
 ORDER BY TongSoNhanVien ASC;
 
--- Li?t k� t�n c�c c�ng ty c� s? nh�n vi�n t? 50 ??n 150 v� t�nh trung b�nh s? nh�n vi�n c?a h?. S?p x?p k?t qu? theo t�n c�ng ty t?ng d?n.
+-- Liệt kê tên các công ty có số nhân viên từ 100 đến 200 và tính trung bình số nhân viên của họ. Sắp xếp kết quả theo tên công ty tăng dần.
 SELECT TenCongTy, AVG(SoNhanVien) AS TrungBinhSoNhanVien
 FROM CongTy
-WHERE SoNhanVien BETWEEN 50 AND 150
+WHERE SoNhanVien BETWEEN 100 AND 200
 GROUP BY TenCongTy
 ORDER BY TenCongTy ASC;
 
--- X�c ??nh s? l??ng k? n?ng cho m?i chuy�n gia c� c?p ?? t?i ?a l� 5 v� ch? bao g?m nh?ng chuy�n gia c� �t nh?t m?t k? n?ng ??t c?p ?? t?i ?a n�y. S?p x?p k?t qu? theo t�n chuy�n gia t?ng d?n.
+-- Xác định số lượng kỹ năng cho mỗi chuyên gia có cấp độ tối đa là 5 và chỉ bao gồm những chuyên gia có ít nhất một kỹ năng đạt cấp độ tối đa này. Sắp xếp kết quả theo tên chuyên gia tăng dần.
 SELECT ChuyenGia.HoTen, COUNT(ChuyenGia_KyNang.MaKyNang) AS SoLuongKyNang
 FROM ChuyenGia
 JOIN ChuyenGia_KyNang ON ChuyenGia.MaChuyenGia = ChuyenGia_KyNang.MaChuyenGia
@@ -121,7 +121,7 @@ GROUP BY ChuyenGia.HoTen
 HAVING COUNT(ChuyenGia_KyNang.MaKyNang) > 0
 ORDER BY ChuyenGia.HoTen ASC;
 
--- Li?t k� t�n c�c k? n?ng m� chuy�n gia c� c?p ?? t? 4 tr? l�n v� t�nh t?ng s? chuy�n gia cho m?i k? n?ng ?�. Ch? bao g?m nh?ng k? n?ng c� t?ng s? chuy�n gia l?n h?n 2. S?p x?p k?t qu? theo t�n k? n?ng t?ng d?n.
+-- Liệt kê tên các kỹ năng mà chuyên gia có cấp độ từ 4 trở lên và tính tổng số chuyên gia cho mỗi kỹ năng đó. Chỉ bao gồm những kỹ năng có tổng số chuyên gia lớn hơn 2. Sắp xếp kết quả theo tên kỹ năng tăng dần.
 SELECT KyNang.TenKyNang, COUNT(ChuyenGia.MaChuyenGia) AS SoLuongChuyenGia
 FROM KyNang
 JOIN ChuyenGia_KyNang ON KyNang.MaKyNang = ChuyenGia_KyNang.MaKyNang
@@ -131,11 +131,11 @@ GROUP BY KyNang.TenKyNang
 HAVING COUNT(ChuyenGia.MaChuyenGia) > 2
 ORDER BY KyNang.TenKyNang ASC;
 
--- T�m ki?m t�n c?a c�c chuy�n gia trong l?nh v?c 'Ph�t tri?n ph?n m?m' v� t�nh trung b�nh c?p ?? k? n?ng c?a h?. Ch? bao g?m nh?ng chuy�n gia c� c?p ?? trung b�nh l?n h?n 3. S?p x?p k?t qu? theo c?p ?? trung b�nh gi?m d?n.
+-- Tìm kiếm tên của các chuyên gia trong lĩnh vực 'Phát triển phần mềm' và tính trung bình cấp độ kỹ năng của họ. Chỉ bao gồm những chuyên gia có cấp độ trung bình lớn hơn 3. Sắp xếp kết quả theo cấp độ trung bình giảm dần.
 SELECT ChuyenGia.HoTen, AVG(ChuyenGia_KyNang.CapDo) 
 FROM ChuyenGia
 JOIN ChuyenGia_KyNang ON ChuyenGia.MaChuyenGia = ChuyenGia_KyNang.MaChuyenGia
-WHERE ChuyenGia.ChuyenNganh = N'Ph�t tri?n ph?n m?m'
+WHERE ChuyenGia.ChuyenNganh = N'Phát triển phần mềm'
 GROUP BY ChuyenGia.HoTen
 HAVING AVG(ChuyenGia_KyNang.CapDo) > 3
 ORDER BY AVG(ChuyenGia_KyNang.CapDo) DESC;
